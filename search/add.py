4611f7cec68549bd17b9e74552ee653267b21fec
Yuvraj Tomar <yuvraj252122gmail.com>
2024-03-29 15:21:57 +0530

    add new file for addition and linear search

---
+++ search/add.py
@@ -0,0 +1,21 @@
+x=23
+y=87
+z=x+y
+print(f"The addition of", x, "+", y, "is", z)
+
+print()
+
+l=[0,1,2,3,34,2267,12]
+e=3
+
+f= False
+for i in l:
+    if(i==l):
+        f=True
+        break
+if(f==True):
+    print("Element found at position")
+    
+else:
+    print("Element not found")
+
